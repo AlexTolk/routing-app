53e3ab6372ba4a1e2d6fa5f41acb8446bae84ee9
Alexander Tolkachev <alexandr.tolkachyov@gmail.com>
2024-10-06 14:33:46 +0800

    edited the redirect page

--- src/components/RedirectPage/RedirectPage.js
+++ src/components/RedirectPage/RedirectPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './RedirectPage.css'
 
 const RedirectPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className='redirect-container'>
       <h1>Redirect Page</h1>
       <button onClick={() => navigate('/')}>Go to Home</button>
     </div>
